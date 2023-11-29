ee307459d9bb066dd45bfc50701975f073bc38c6
Ben Püschel <ben.pueschel2004@gmail.com>
2023-11-29 04:32:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# scripts
+Some useful bash scripts
+
+## walk-dir.sh
+```sh ./walk-dir.sh [opts] <dir>```
+Recursively walk the specified directory and print all file paths to stdout.
+### Options
+- `-a`: show hidden files
---
+++ walk-dir.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+all_files=false
+
+while getopts a: flag
+do
+	case "${flag}" in
+		a) all_files=true;;
+	esac
+done
+
+list_files() {
+	while IFS= read -r file; do
+		if [[ -z "$file" || "$file" = "." || "$file" = ".." ]]; then # if line is empty or '.' or '..', skip
+			continue
+		fi
+		FILE="$1/$file"
+		if [ -d "$FILE" ]; then
+			list_files $FILE
+		else
+			printf '%s\n' $FILE
+		fi
+#	done <<< ""
+	done <<< "$(ls $(if $all_files; then echo "-a"; fi) $1)"
+}
+
+list_files ${@: -1} # list files using the last cli argument
