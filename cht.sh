39040a655273f6b66496e3e882ff8a41065fafa1
Ben Püschel <ben.pueschel2004@gmail.com>
2023-12-05 15:44:39 +0100

    add cht.sh script

---
+++ cht.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+languages=`echo "golang lua cpp c rust kotlin" | tr ' ' '\n'`
+
+selected=`printf "$languages\n" | fzf`
+read -p "query: " query
+
+tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
